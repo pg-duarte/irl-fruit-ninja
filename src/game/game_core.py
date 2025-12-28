6e62b706eba82cbb101533fa4568ccef6269171f
Pedro Duarte <pag.duarte@campus.fct.unl.pt>
2025-12-28 15:29:03 +0000

    Add initial game core structure

---
+++ src/game/game_core.py
@@ -0,0 +1,10 @@
+class Game:
+    def __init__(self):
+        self.score = 0
+        self.fruits = []
+
+    def update(self, dt, hands, trails):
+        pass
+
+    def draw(self, frame):
+        return frame
